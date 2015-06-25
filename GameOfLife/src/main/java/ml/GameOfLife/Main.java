42d234b47f9d7ab235701ae2937a83af0e4d9138
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-06-25 16:51:11 +0200

    New bigger boards configuration tested.

--- GameOfLife/src/main/java/ml/GameOfLife/Main.java
+++ GameOfLife/src/main/java/ml/GameOfLife/Main.java
@@ -2,21 +2,31 @@ package ml.GameOfLife;
 
 public class Main {
 	public static void main(String[] args) {
-		boolean [][] board = {{true, false, true}, {false, true, false}, {true, false, true}};
+//		boolean [][] board = {{false, false, false, false}, {false, true, true, false},
+//				{false, true, true, false}, {false, false, false, false}};
+//		boolean [][] board = {{false, false, false, false}, {false, true, true, true},
+//				{false, false, false, false}, {false, false, false, false}};
+//		boolean [][] board = {{false, true, true, true}, {false, true, true, true},
+//				{false, false, true, false}, {false, false, false, false}};
+//		boolean [][] board = {{false, true, false, true}, {false, true, true, false},
+//				{true, false, true, false}, {true, true, false, false}};
+		boolean [][] board = {{false, true, false, true, true}, {false, true, true, false, true},
+//				{true, false, true, false, false}, {true, true, false, true, true},
+				{true, true, false, false, true}};
+		
 		GameOfLife gameOfLife = new GameOfLife(board);
 		Viewer view = new GameOfLifeViewer(gameOfLife);	 
         
 		while(true) {
+			for (int i=1; i<=5 ; i++)
+				System.out.println("\n");
 			view.prt();
 			gameOfLife.updateGame();
 			try {
-			Thread.sleep(500);
+			Thread.sleep(700);
 			} 
 			catch(InterruptedException e) {
 			} 
-			for (int i=1; i<=10 ; i++)
-			    System.out.println("\n");
-//			System.out.println('\r');
 		}
 	}
 }
